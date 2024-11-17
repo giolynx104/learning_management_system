62f30d7bde46c161fc45945302d6ce11f8d6e6e7
Giao Le <giolynx104@gmail.com>
2024-11-17 12:19:01 +0700

    Fix mismatching class model

--- lib/models/class_model.dart
+++ lib/models/class_model.dart
@@ -10,12 +10,13 @@ class ClassModel with _$ClassModel {
     @JsonKey(name: 'class_name') required String className,
     @JsonKey(name: 'attached_code') String? attachedCode,
     @JsonKey(name: 'class_type') required String classType,
-    @JsonKey(name: 'lecturer_name') required String lecturerName,
-    @JsonKey(name: 'student_count') required int studentCount,
+    required int id,
+    @JsonKey(name: 'lecturer_id') required int lecturerId,
+    @JsonKey(name: 'max_student_amount') required int maxStudentAmount,
+    String? schedule,
     @JsonKey(name: 'start_date') required String startDate,
     @JsonKey(name: 'end_date') required String endDate,
     required String status,
-    @JsonKey(name: 'student_accounts') required List<dynamic> studentAccounts,
   }) = _ClassModel;
 
   factory ClassModel.fromJson(Map<String, dynamic> json) => _$ClassModelFromJson(json);
